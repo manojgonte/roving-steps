598d40421823ba0a9cab1428471f4526cea1183b
ishwarsathe <ishwarsathe67@gmail.com>
2023-04-21 23:16:24 +0530

    Merging changes of Manoj

--- src/pages/Header/header.jsx
+++ src/pages/Header/header.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link"
 
 export const Header = () => {
     return (
@@ -18,10 +19,12 @@ export const Header = () => {
                 <div className="border-[#ECA740] h-full w-fit flex justify-center items-center font-roboto font-normal opacity-100 text-xs hover:border-b-2">GALLERY</div>
                 <div className="border-[#ECA740] h-full w-fit flex justify-center items-center font-roboto font-normal opacity-100 text-xs hover:border-b-2">FLIGHT BOOKING</div>
                 <div className="border-[#ECA740] h-full w-fit flex justify-center items-center font-roboto font-normal opacity-100 text-xs hover:border-b-2">OTHER SERVICES</div>
-                <div className="border-[#ECA740] h-full w-fit flex justify-center items-center font-roboto font-normal opacity-100 text-xs hover:border-b-2">CONTACT US</div>
+                <div className="border-[#ECA740] h-full w-fit flex justify-center items-center font-roboto font-normal opacity-100 text-xs hover:border-b-2">
+                    <Link href="/Contact">CONTACT US</Link>
+                </div>
                 <div className=" h-full w-fit flex justify-center items-center">
                     <button className="bg-[#ECBF40] w-full h-10 p-3 flex items-center justify-center rounded-md font-roboto font-normal opacity-100 text-xs hover:shadow-lg">
-                        SIGN UP
+                        <Link href="/Register">SIGN UP</Link>
                     </button>
                 </div>
             </div>
