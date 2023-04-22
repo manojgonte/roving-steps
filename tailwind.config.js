79dc3f0c804a23b44f14a6d95a19e9614570d48a
manojg8 <manoj@ycstech.in>
2023-04-22 18:09:53 +0530

    tailwind config naming changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/component/**/*.{js,ts,jsx,tsx}',
+    './src/Component/**/*.{js,ts,jsx,tsx}',
     './src/app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
