1b49416785b25d794891719aa4b6e64164c7b7ba
manojg8 <manoj@ycstech.in>
2023-05-17 00:33:12 +0530

    UI refinement  tour detail + galley page

--- src/pages/Gallery/Gallery.js
+++ src/pages/Gallery/Gallery.js
@@ -2,10 +2,11 @@ import React from 'react'
 import Header from '../Header'
 import Image from 'next/image'
 import Footer from '../Footer'
+import SearchFilter from '@/Component/Common/SearchFilter/SearchFilter'
 
 export default function Gallery() {
     return (
-        <main className="bg-gradient-to-l from-cyan-200 to-white">
+        <main className="bg-white" style={{backgroundImage:'url(Assets/images/elements/bg_element.png)', backgroundPosition:'center'}}>
             <Header />
             <div className="flex flex-col relative w-full h-[350px] justify-center items-center gap-8 bg-scroll banner-image px-16">
                 <Image
@@ -25,58 +26,54 @@ export default function Gallery() {
                             Gallery
                         </span>
                     </div>
-                    <div className="w-11/12 h-9 border">
-                        <div className="bg-white w-full h-full rounded-3xl flex items-center">
-                            <div className="w-1/5 border-[#ECBF40] h-2/3 flex items-center rounded-l-3xl border-r-2 my-2 ml-2 pl-4 gap-1">
-                                <Image
-                                    src={"/Assets/images/icons/LocationDefault.svg"}
-                                    width={10}
-                                    height={10}
-                                    alt=""
-                                />
-                                <input placeholder="Location" className="p-1 rounded-l-3xl w-full  text-sm" />
-                            </div>
-                            <div className="w-1/6 border-[#ECBF40] h-2/3 flex items-center rounded-l-3xl border-r-2 my-2 ml-2 pl-4 gap-1">
-                                <Image
-                                    src={"/Assets/images/icons/DateDefault.svg"}
-                                    width={15}
-                                    height={15}
-                                    alt=""
-                                />
-                                <input placeholder="Check in" className="p-1 w-full  text-sm" />
-                            </div>
-                            <div className="w-1/6 border-[#ECBF40] h-2/3 flex items-center rounded-l-3xl border-r-2 my-2 ml-2 pl-4 gap-1">
-                                <Image
-                                    src={"/Assets/images/icons/DateDefault.svg"}
-                                    width={15}
-                                    height={15}
-                                    alt=""
-                                />
-                                <input placeholder="Check out" className="p-1 w-full  text-sm" />
-                            </div>
-                            <div className="w-1/6 border-[#ECBF40] h-2/3 flex items-center rounded-l-3xl border-r-2 my-2 ml-2 pl-4 gap-1">
-                                <Image
-                                    src={"/Assets/images/icons/DateDefault.svg"}
-                                    width={15}
-                                    height={15}
-                                    alt=""
-                                />
-                                <input placeholder="Adult" className="w-full p-1  text-sm" />
-                            </div>
-                            <div className="w-1/6 border-[#ECBF40] h-2/3 flex items-center rounded-l-3xl border-r-2 my-2 ml-2 pl-4 gap-1">
-                                <Image
-                                    src={"/Assets/images/icons/DateDefault.svg"}
-                                    width={15}
-                                    height={15}
-                                    alt=""
-                                />
-                                <input placeholder="Kids" className="w-full p-1  text-sm" />
-                            </div>
-                            <div className="w-1/6 h-full flex items-center pl-6 rounded-r-3xl hover:bg-[#ECBF40]">
-                                <button className="text-[#C6C6C6] hover:text-white  text-sm">
-                                    Search
-                                </button>
-                            </div>
+                    <SearchFilter />
+                </div>
+            </div>
+
+            <div className="px-16 py-10">
+                <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+                    <div className="grid gap-4">
+                        <div>
+                            <img className="h-auto max-w-full rounded-lg object-cover" src="/Assets/images/gallery/image1.jpg" alt="" />
+                        </div>
+                        <div>
+                            <img className="h-auto max-w-full rounded-lg object-cover" src="/Assets/images/gallery/image2.jpg" alt="" />
+                        </div>
+                        <div>
+                            <img className="h-auto max-w-full rounded-lg object-cover" src="/Assets/images/gallery/image3.jpg" alt="" />
+                        </div>
+                    </div>
+                    <div className="grid gap-4">
+                        <div>
+                            <img className="h-auto max-w-full rounded-lg" src="/Assets/images/gallery/image4.jpg" alt="" />
+                        </div>
+                        <div>
+                            <img className="h-auto max-w-full rounded-lg" src="/Assets/images/gallery/image5.jpg" alt="" />
+                        </div>
+                        <div>
+                            <img className="h-auto max-w-full rounded-lg" src="/Assets/images/gallery/image6.jpg" alt="" />
+                        </div>
+                    </div>
+                    <div className="grid gap-4">
+                        <div>
+                            <img className="h-auto max-w-full rounded-lg" src="/Assets/images/gallery/image7.jpg" alt="" />
+                        </div>
+                        <div>
+                            <img className="h-auto max-w-full rounded-lg" src="/Assets/images/gallery/image8.jpg" alt="" />
+                        </div>
+                        <div>
+                            <img className="h-auto max-w-full rounded-lg" src="/Assets/images/gallery/image9.jpg" alt="" />
+                        </div>
+                    </div>
+                    <div className="grid gap-4">
+                        <div>
+                            <img className="h-auto max-w-full rounded-lg" src="/Assets/images/gallery/image10.jpg" alt="" />
+                        </div>
+                        <div>
+                            <img className="h-auto max-w-full rounded-lg" src="/Assets/images/gallery/image11.jpg" alt="" />
+                        </div>
+                        <div>
+                            <img className="h-auto max-w-full rounded-lg" src="/Assets/images/gallery/image12.jpg" alt="" />
                         </div>
                     </div>
                 </div>
