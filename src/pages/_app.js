0c63c8e41250e8156d3e47f15206d4659bf161c9
manojg8 <manoj@ycstech.in>
2023-06-04 14:32:30 +0530

    basic SEO

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,13 +2,14 @@ import '@/styles/globals.css'
 import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <>
-    <Head>
-      <meta
-        name='viewport'
-        content='width=device-width, intial-scale=1'
-      />
-    </Head>
-    <Component {...pageProps} />
-  </>
+    return <>
+        <Head>
+            <title>Roving Steps</title>
+            <meta name='viewport' content='width=device-width, intial-scale=1' />
+            <link rel="shortcut icon" href="/Assets/images/favicon.png"></link>
+            <meta name='keywords' content='Roving Steps' />
+            <meta name='description' content='Roving Steps' />
+        </Head>
+        <Component {...pageProps} />
+    </>
 }
